87506a6cb63f867fdfc3f6949ad4c6f1e27e0f97
balazs4 <balazs4web@gmail.com>
2024-03-27 10:24:42 +0100

    inject html

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-  h := http.NewServeMux()
-	h.HandleFunc("GET /{file}", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("GET /{file}", func(w http.ResponseWriter, r *http.Request) {
 		name := r.PathValue("file")
-		http.ServeFile(w, r, name)
+		if strings.HasSuffix(name, ".html") == false {
+			http.ServeFile(w, r, name)
+			return
+		}
+
+		w.Write([]byte(fmt.Sprintf("<h2>%s</h2>", time.Now())))
+		file, err := os.Open(name)
+		if err != nil {
+			return
+		}
+		file.WriteTo(w)
 	})
-  http.ListenAndServe(":3000", h)
+	http.ListenAndServe(":3000", nil)
 }
