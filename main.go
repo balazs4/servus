577e479fccedc9e1c6eb5058513355cdb2c8edaa
balazs4 <balazs4web@gmail.com>
2024-03-28 10:56:53 +0100

    yolo commit :sponge:

--- main.go
+++ main.go
@@ -5,11 +5,28 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
-	"time"
+	"syscall"
 )
 
 func main() {
+	signalchan := make(chan os.Signal, 1)
+	signal.Notify(signalchan, syscall.SIGUSR2)
+
+	script := `<script> new EventSource('/.servus').onmessage = function(){ console.log("data"); location.reload(); }</script>`
+	http.HandleFunc("GET /.servus", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Add("content-type", "text/event-stream")
+		w.Header().Add("connection", "keep-alive")
+		w.Header().Add("cache-control", "no-cache")
+
+		<-signalchan
+    fmt.Println("SIGUSR2");
+		data := fmt.Sprintf("data: servus pid %d\n\n", os.Getpid())
+		w.Write([]byte(data))
+	})
+
 	http.HandleFunc("GET /{file}", func(w http.ResponseWriter, r *http.Request) {
 		name := r.PathValue("file")
 		if strings.HasSuffix(name, ".html") == false {
@@ -21,9 +38,10 @@ func main() {
 		if err != nil {
 			return
 		}
-		w.Write([]byte(fmt.Sprintf("<h2>%s</h2>", time.Now())))
+		w.Write([]byte(script))
 		io.Copy(w, file)
 	})
+	fmt.Printf("pid=%d url=http://localhost:3000", os.Getpid())
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		panic(err)
---
+++ makefile
@@ -0,0 +1,11 @@
+fmt:
+	go fmt .
+
+run:
+	go run .
+
+build:
+	go build .
+
+clean:
+	go clean .
